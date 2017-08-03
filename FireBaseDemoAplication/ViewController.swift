a09e30e6838f2828dadc0d980fb8afca7a641896
JonathanSaldivarMendoza <saldivar.jonathan@hotmail.com>
2017-08-03 11:20:15 -0500

    download data and show'

--- FireBaseDemoAplication/ViewController.swift
+++ FireBaseDemoAplication/ViewController.swift
@@ -7,22 +7,57 @@
 //
 
 import UIKit
-import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     
-    var ref: DatabaseReference!
-
+    var ref: DatabaseReference?
+    var handle: DatabaseHandle?
+    var dataSource: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ref = Database.database().reference()
+        handle = ref?.child("list").observe(.childAdded, with: { (snapshot) in
+            if let item = snapshot.value as? String {
+                self.dataSource.append(item)
+                self.tableView.reloadData()
+            }
+        })
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    // Saving item into a database
+    
+    @IBAction func sendButtomPress(_ sender: Any) {
+        
+        ref?.child("list").childByAutoId().setValue(textField.text)
+        
+        if textField.text != nil{
+            
+            textField.text = ""
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selecciono \(indexPath.row)")
+    }
 
 }
 
